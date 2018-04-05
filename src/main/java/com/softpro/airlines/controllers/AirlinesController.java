291bdd2eb7107d96e5df9dbc5587ef4dfbcbb6c2
Evgeniy Amosov <efreet@efreet.ru>
2018-04-05 11:39:55 +0300

    comments

--- src/main/java/com/softpro/airlines/controllers/AirlinesController.java
+++ src/main/java/com/softpro/airlines/controllers/AirlinesController.java
@@ -23,17 +23,39 @@ public class AirlinesController {
     @Autowired
     private AirlinesService airlinesService;
 
+    /**
+     * Получить самолет по id
+     * @param id
+     * @return
+     * @throws AirplainNotFoundException
+     */
     @RequestMapping("/airplains/{id}")
     public Airplain getAirplain(@PathVariable(value = "id") String id) throws AirplainNotFoundException {
         return airlinesService.airplain(Long.parseLong(id));
     }
 
+    /**
+     * Посчитать все посадки самолета airplain_id в аэропорту airport_id
+     * @param airplainId
+     * @param airportId
+     * @return
+     * @throws AirplainNotFoundException
+     */
     @RequestMapping("/lands/{airplain_id}/{airport_id}")
     public long countLands(@PathVariable(value = "airplain_id") String airplainId,
                            @PathVariable(value = "airport_id") String airportId) throws AirplainNotFoundException {
         return airlinesService.countLands(Long.parseLong(airplainId), Long.parseLong(airportId));
     }
 
+    /**
+     * Посчитать все посадки самолета airplain_id в аэропорту airport_id за дни между start и finish
+     * @param airplainId
+     * @param airportId
+     * @param start
+     * @param finish
+     * @return
+     * @throws AirplainNotFoundException
+     */
     @RequestMapping("/lands/{airplain_id}/{airport_id}/{start}/{finish}")
     public long countLands(@PathVariable(value = "airplain_id") String airplainId,
                            @PathVariable(value = "airport_id") String airportId,
@@ -46,16 +68,30 @@ public class AirlinesController {
                                           LocalDate.parse(finish));
     }
 
+    /**
+     * Получить список всех самолетов
+     * @return
+     */
     @RequestMapping("/airplains")
     public List<Airplain> getAirplain() {
         return airlinesService.airplains();
     }
 
+    /**
+     * Получить Аэропорт по id
+     * @param id
+     * @return
+     * @throws AirportNotFoundException
+     */
     @RequestMapping("/airports/{id}")
     public Airport getAirport(@PathVariable(value = "id") String id) throws AirportNotFoundException {
         return airlinesService.airport(Long.parseLong(id));
     }
 
+    /**
+     * Получить список всех Аэропортов
+     * @return
+     */
     @RequestMapping("/airports")
     public List<Airport> getAirports() {
         return airlinesService.airports();
@@ -66,21 +102,44 @@ public class AirlinesController {
         return airlinesService.route(Long.parseLong(id));
     }
 
+    /**
+     * Получить список всех маршрутов
+     * @return
+     */
     @RequestMapping("/routes")
     public List<Route> getRoutes() {
         return airlinesService.routes();
     }
 
+    /**
+     * Получить Полет по его id
+     * @param id
+     * @return
+     * @throws FlightNotFoundException
+     */
     @RequestMapping("/flights/{id}")
     public Flight getFlight(@PathVariable(value = "id") String id) throws FlightNotFoundException {
         return airlinesService.flight(Long.parseLong(id));
     }
 
+    /**
+     * Получить список всех полетов
+     * @return
+     */
     @RequestMapping("/flights")
     public List<Flight> getFlights() {
         return airlinesService.flights();
     }
 
+    /**
+     * Добавить полет самолета airplain_id по маршуруту route_id в день date
+     * @param date
+     * @param airplainId
+     * @param routeId
+     * @return
+     * @throws RouteNotFoundException
+     * @throws AirplainNotFoundException
+     */
     @RequestMapping("/flights/add/{date}/{airplain_id}/{route_id}")
     public Flight addFlight(@PathVariable("date") String date,
                             @PathVariable("airplain_id") String airplainId,
